5f1c3b9410caac77ed5dcce65630ff63169751d6
chintu kumar <chintudas499@gmail.com>
2026-04-06 12:38:53 +0530

    second largest element in given array

---
+++ src/Array/LargestInArray.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class LargestInArray {
+    static void main() {
+        int arr [] = new int[5];
+        int max = 0;
+        int Smax= 0;
+
+       arr = new int[]{11,12,5,34,5};
+       arr[0] = max;
+
+       for(int i = 1 ;i<arr.length;i++){
+          if(arr[i]>max){
+              Smax = max;
+              max = arr[i];
+          }
+          else if(arr[i]>Smax && arr[i]!=max){
+              Smax = arr[i];
+          }
+       }
+        System.out.println(max);
+        System.out.println(Smax);
+    }
+}
